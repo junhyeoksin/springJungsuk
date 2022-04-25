4b013df1bd41e2e7768872514c070082de646aa3
junhyeoksin <h-kun@naver.com>
2022-04-26 00:05:31 +0900

    RateDiscountPolicy

---
+++ src/main/java/com/study/springjungsuk/springIprun/discount/RateDiscountPolicy.java
@@ -0,0 +1,25 @@
+package com.study.springjungsuk.springIprun.discount;
+
+import com.study.springjungsuk.springIprun.member.Grade;
+import com.study.springjungsuk.springIprun.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+
+    private int discountPercent = 10;
+    /**
+     *  고정할인 금액이 아니라 정률 할인 ex) 10000원 구매시 1000원 할인
+      * @param member
+     * @param price
+     * @return
+     */    
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
