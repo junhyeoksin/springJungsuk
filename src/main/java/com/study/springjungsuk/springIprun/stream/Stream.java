4164bd3e3ac296d9af7c9be726ccf1a8bdd25aef
junhyeoksin <92718453+junhyeoksin@users.noreply.github.com>
2022-05-01 14:14:55 +0900

    update: stream, list, set

--- src/main/java/com/study/springjungsuk/springIprun/stream/Stream.java
+++ src/main/java/com/study/springjungsuk/springIprun/stream/Stream.java
@@ -1,9 +1,6 @@
 package com.study.springjungsuk.springIprun.stream;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
@@ -37,19 +34,12 @@ public class Stream {
         System.out.println("paralleStream === " + paralleStream);
     }
 
+
+
+    
     /**
-     * 스트림은 한번만 소비가 가능하다.
+     *  
      */
-    static void impossibleNewStream(){
-        List<String> title = Arrays.asList("a", "b", "c", "d", "e", "f");
-
-
-        java.util.stream.Stream<String> stream = title.stream();
-//      stream.forEach(System.out::println);
-        stream.forEach(name -> System.out.println("name = " + name));  // Lambda 로 표현
-
-    }
-
     static void ExCosumerNewStream(){
         List<String> s = new ArrayList<>();
         s.add("one");
@@ -65,4 +55,36 @@ public class Stream {
             }
         });
     }
+    /**
+     * 리스트 인덱스로 꺼내기  list: List는 중복을 허용 즉, 중복된 아이템을 List에 추가할 수 있습니다.
+     *
+     * 스트림은 한번만 소비가 가능하다.
+     * 1. Lambda 사용으로 간결하게 사용
+     * 2. 메소드 레퍼런스 사용 
+     */
+    static void impossibleNewStream(){
+        List<String> list = Arrays.asList("a", "b", "c", "d", "e", "f");
+
+        String s = list.get(0);
+        String s1 = list.get(1);
+        String s2 = list.get(2);
+        System.out.println("s = " + s);
+        System.out.println("s1 = " + s1);
+        System.out.println("s2 = " + s2);
+        System.out.println("s -- "+ s +  ", s1----" +  s1 +  ", s2----" +  s2);
+
+        java.util.stream.Stream<String> stream = list.stream();
+        stream.forEach(name -> System.out.println("name = " + name));  // Lambda 로 표현
+        //      stream.forEach(System.out::println);   // 메소드 레퍼런스 사용
+    }
+
+    /**
+     * 저장되는 데이터의 순서를 보장해야한다면 List를 사용
+     * contains(element)는 Collection에 데이터가 존재하는지 확인하는 메소드. List의 contains 실행 속도는 O(n)이지만, Set는 O(1)으로 매우 빠름. 탐색이 잦다면 Set를 고려.
+     * 데이터가 많지 않다면 성능보다, 구조가 간단한 List를 고려.
+     * 중복을 허용하지 않는 Collection이 필요하다면 Set를 고려.
+     */
+
+
+
 }
